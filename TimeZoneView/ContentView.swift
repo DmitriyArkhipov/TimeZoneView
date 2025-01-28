e2c92105ac74c941a70dd1d1cd0590206cdc366c
Dmitriy Arkhipov <dmitriy.arkhipov.biz@mail.ru>
2025-01-28 18:57:05 +0700

    Initial Commit

---
+++ TimeZoneView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeZoneView
+//
+//  Created by Дмитрий Архипов on 28.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeZoneView/TimeZoneViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeZoneViewApp.swift
+//  TimeZoneView
+//
+//  Created by Дмитрий Архипов on 28.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TimeZoneViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
